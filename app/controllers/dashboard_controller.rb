c02e577d5cd4b3779730acd462d3c18e5537f20a
belammuia <belammuia0@gmail.com>
2024-09-25 12:47:59 +0300

    changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,5 @@
 class DashboardController < ApplicationController
-    def index
-        
+    def index     
     end 
+
 end
--- app/helpers/tasks_helper.rb
+++ app/helpers/tasks_helper.rb
@@ -1,5 +1,2 @@
 module TasksHelper
-    def render_error(task)
-        render json: { error: @task.errors.full_messages },status: :unprocessable_entity
-    end
 end
