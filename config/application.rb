f3499c85aeb0b428c0a2bcdf26f81e59dcf87ed8
belammuia <belammuia0@gmail.com>
2024-09-27 23:41:34 +0300

    protect secrets.yml

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,10 @@ require "rails/all"
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+if Rails.env.development? || Rails.env.test?
+  Dotenv::Rails.load
+end
+
 module Todolist
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
