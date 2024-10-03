db677eecbba4cc026e4cda33f42013ddeb76d211
belam <167896330+bmuia@users.noreply.github.com>
2024-10-03 19:58:06 +0300

    Update sessions_controller.rb

--- app/controllers/users/sessions_controller.rb
+++ app/controllers/users/sessions_controller.rb
@@ -4,6 +4,6 @@ class Users::SessionsController < Devise::SessionsController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path 
+    stored_location_for(resource_or_scope) || "/"
   end
 end
