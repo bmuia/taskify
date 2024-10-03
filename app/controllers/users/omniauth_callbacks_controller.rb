fa1d8d02efcf3244ad95b6c282ad0ea5507bdfa2
belam <167896330+bmuia@users.noreply.github.com>
2024-10-03 19:57:41 +0300

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -17,7 +17,7 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   end
 
   def after_sign_in_path_for(resource_or_scope)
-    stored_location_for(resource_or_scope) || root_path
+    stored_location_for(resource_or_scope) ||"/"
   end
 
   private
